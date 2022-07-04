a8bfbdd6cd9601e263a64461f1c97110182115e5
Pavel Khramov <paul.khramov@gmail.com>
2022-07-04 22:33:45 +0500

    feature: ClientApi added

--- frontend/views/companies.py
+++ frontend/views/companies.py
@@ -1,16 +1,12 @@
 from flask import Blueprint, render_template
 
-from frontend.api.companies import CompaniesApi
-from frontend.config import config
-
-companies_api = CompaniesApi(config.backend.url)
-
+from frontend.api.client import client
 
 routes = Blueprint('companies', __name__)
 
 
 @routes.get('/companies')
 def get_all():
-    companies = companies_api.get_all()
+    companies = client.companies.get_all()
 
     return render_template('companies.html', page_title='Список компаний', companies=companies)
